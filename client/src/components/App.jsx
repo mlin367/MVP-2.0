7d05c34bb1b6c362a191129653e42805961f60a1
Matthew Lin <mlin367@yahoo.com>
2019-03-25 19:04:25 -0700

    made some CSS adjustments with whole body using flex correctly

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -176,31 +176,26 @@ class App extends React.Component {
           <h3>White has won: {this.state.whiteWin} times</h3>
         </div>
         <div className={styles.boardWin}>
-        <Board
-          boardState={this.state.boardState}
-          handleOnClick={this.handleOnClick}
-          currentColor={this.state.currentColor}
-          size={this.state.size}
-        />
-        {this.state.victory ? (
-          <VictoryPage
-            victor={this.state.currentColor === this.black ? 'White' : 'Black'}
+          <Board
+            boardState={this.state.boardState}
+            handleOnClick={this.handleOnClick}
+            currentColor={this.state.currentColor}
+            size={this.state.size}
           />
-        ) : null}
 
         </div>
+        <h1 className={styles.turn}>
+            {this.state.victory ? null: this.whosTurn()}
+          </h1>
+          {this.state.victory ? (
+            <VictoryPage
+              victor={this.state.currentColor === this.black ? 'White' : 'Black'}
+            />
+          ) : null}
         <div className={styles.buttons}>
           <button onClick={this.buttonsOnClick} className="clearWin"> Clear Win Record</button>
           <button onClick={this.buttonsOnClick} className="clearBoard"> Clear Board</button>
         </div>
-        <div className={styles.turn}>
-          {this.state.victory ? null: this.whosTurn()}
-        </div>
-        {/* {this.state.victory ? (
-          <VictoryPage
-            victor={this.state.currentColor === this.black ? 'White' : 'Black'}
-          />
-        ) : null} */}
       </div>
     );
   }
